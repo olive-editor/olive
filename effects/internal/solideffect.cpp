af24b283545b1cd41dc5790e4e3c376a423a589e
itsmattkc <itsmattkc@gmail.com>
2018-11-20 09:27:34 +1100

    revised checkerboard

--- effects/internal/solideffect.cpp
+++ effects/internal/solideffect.cpp
@@ -5,6 +5,7 @@
 #include <QtMath>
 #include <QVariant>
 #include <QImage>
+#include <QComboBox>
 
 #include "project/clip.h"
 #include "project/sequence.h"
@@ -26,37 +27,31 @@ SolidEffect::SolidEffect(Clip* c, const EffectMeta* em) : Effect(c, em) {
     solid_type->add_combo_item("Checkerboard", SOLID_TYPE_CHECKERBOARD);
 	solid_type->id = "type";
 
+    opacity_field = add_row("Opacity:")->add_field(EFFECT_FIELD_DOUBLE);
+    opacity_field->set_double_minimum_value(0);
+    opacity_field->set_double_maximum_value(100);
+    opacity_field->set_double_default_value(100);
+    opacity_field->id = "opacity";
+
 	solid_color_field = add_row("Color:")->add_field(EFFECT_FIELD_COLOR);
 	solid_color_field->set_color_value(Qt::red);
 	solid_color_field->id = "color";
 
-	opacity_field = add_row("Opacity:")->add_field(EFFECT_FIELD_DOUBLE);
-	opacity_field->set_double_minimum_value(0);
-	opacity_field->set_double_maximum_value(100);
-	opacity_field->set_double_default_value(100);
-	opacity_field->id = "opacity";
-
-    solid_width_field = add_row("Width:")->add_field(EFFECT_FIELD_DOUBLE);
-    solid_width_field->set_double_minimum_value(1);
-    solid_width_field->set_double_maximum_value(c->getWidth());
-    solid_width_field->set_double_default_value(c->getWidth() / 10);
-    solid_width_field->id = "width";
+    checkerboard_size_field = add_row("Checkerboard Size:")->add_field(EFFECT_FIELD_DOUBLE);
+    checkerboard_size_field->set_double_minimum_value(1);
+    checkerboard_size_field->set_double_default_value(10);
+    checkerboard_size_field->id = "checker_size";
 
     connect(solid_type, SIGNAL(changed()), this, SLOT(field_changed()));
 	connect(solid_color_field, SIGNAL(changed()), this, SLOT(field_changed()));
 	connect(opacity_field, SIGNAL(changed()), this, SLOT(field_changed()));
-    connect(solid_width_field, SIGNAL(changed()), this, SLOT(field_changed()));
+    connect(checkerboard_size_field, SIGNAL(changed()), this, SLOT(field_changed()));
+
+    connect(static_cast<QComboBox*>(solid_type->get_ui_element()), SIGNAL(currentIndexChanged(int)), this, SLOT(ui_update(int)));
 
 	vertPath = ":/shaders/common.vert";
 	fragPath = ":/shaders/solideffect.frag";
 }
-
-/*void SolidEffect::process_gl(double timecode, GLTextureCoords&) {
-
-
-	glslProgram->setUniformValue("solidColor", solid_color_field->get_color_value(timecode));
-	glslProgram->setUniformValue("amount_val", (GLfloat) (opacity_field->get_double_value(timecode)*0.01));
-}*/
 
 void SolidEffect::redraw(double timecode) {
 	int w = img.width();
@@ -158,122 +153,31 @@ void SolidEffect::redraw(double timecode) {
     {
         // draw checkboard
         QPainter p(&img);
-        p.setRenderHint(QPainter::Antialiasing);
         img.fill(Qt::transparent);
 
-        int checker_width = qCeil(solid_width_field->get_double_value(timecode));
-        int checker_x, checker_y, count = 0;
+        int checker_width = qCeil(checkerboard_size_field->get_double_value(timecode));
+        int checker_x, checker_y;
         int checkerboard_size_w = qCeil(double(w)/checker_width);
         int checkerboard_size_h = qCeil(double(h)/checker_width);
 
-        QColor checker_odd(QColor(0,0,0,alpha));
+        QColor checker_odd(QColor(0, 0, 0, alpha));
         QColor checker_even(solid_color_field->get_color_value(timecode));
         checker_even.setAlpha(alpha);
         QVector<QColor> checker_color{checker_odd, checker_even};
 
         for(int i = 0; i < checkerboard_size_w; i++){
             checker_x = checker_width*i;
-            if (checkerboard_size_h % 2 == 0 && count > 0) count++;
             for(int j = 0; j < checkerboard_size_h; j++){
-                  checker_y = checker_width*j;
-                  p.fillRect(QRect(checker_x, checker_y, checker_width, checker_width), checker_color[count%2]);
-                  count++;
+                checker_y = checker_width*j;
+                p.fillRect(QRect(checker_x, checker_y, checker_width, checker_width), checker_color[(i + j)%2]);
             }
         }
     }
         break;
-	}
+    }
 }
 
-/*void SolidEffect::process_image(long frame, uint8_t* data, int w, int h) {
-	QImage img(data, w, h, QImage::Format_RGBA8888); // create QImage wrapper
-	QPainter p(&img);
-	int width = img.width();
-	int height = img.height();
-
-	switch (solid_type->get_combo_data(frame).toInt()) {
-	case SOLID_TYPE_COLOR:
-	{
-		QColor brush = solid_color_field->get_color_value(frame);
-		p.fillRect(0, 0, width, height, QColor(brush.red(), brush.green(), brush.blue(), opacity_field->get_double_value(frame)*2.55));
-	}
-		break;
-	case SOLID_TYPE_BARS:
-		// draw smpte bars
-		int bar_width = qCeil((double) parent_clip->sequence->width / 7.0);
-		int first_bar_height = qCeil((double) parent_clip->sequence->height / 3.0 * 2.0);
-		int second_bar_height = qCeil((double) parent_clip->sequence->height / 12.5);
-		int third_bar_y = first_bar_height + second_bar_height;
-		int third_bar_height = parent_clip->sequence->height - third_bar_y;
-		int third_bar_width = 0;
-		int bar_x, strip_width;
-		QColor first_color, second_color, third_color;
-		for (int i=0;i<SMPTE_BARS;i++) {
-			bar_x = bar_width*i;
-			switch (i) {
-			case 0:
-				first_color = QColor(192, 192, 192);
-				second_color = QColor(0, 0, 192);
-				break;
-			case 1:
-				first_color = QColor(192, 192, 0);
-				second_color = QColor(19, 19, 19);
-				break;
-			case 2:
-				first_color = QColor(0, 192, 192);
-				second_color = QColor(192, 0, 192);
-				break;
-			case 3:
-				first_color = QColor(0, 192, 0);
-				second_color = QColor(19, 19, 19);
-				break;
-			case 4:
-				first_color = QColor(192, 0, 192);
-				second_color = QColor(0, 192, 192);
-				break;
-			case 5:
-				third_bar_width = bar_x / SMPTE_LOWER_BARS;
-
-				first_color = QColor(192, 0, 0);
-				second_color = QColor(19, 19, 19);
-
-				strip_width = qCeil(bar_width/SMPTE_STRIP_COUNT);
-				for (int j=0;j<SMPTE_STRIP_COUNT;j++) {
-					switch (j) {
-					case 0:
-						third_color = QColor(29, 29, 29);
-						p.fillRect(QRect(bar_x, third_bar_y, bar_width, third_bar_height), third_color);
-						break;
-					case 1:
-						third_color = QColor(19, 19, 19);
-						p.fillRect(QRect(bar_x + strip_width, third_bar_y, strip_width, third_bar_height), third_color);
-						break;
-					case 2:
-						third_color = QColor(9, 9, 9);
-						p.fillRect(QRect(bar_x, third_bar_y, strip_width, third_bar_height), third_color);
-						break;
-					}
-				}
-				break;
-			case 6:
-				first_color = QColor(0, 0, 192);
-				second_color = QColor(192, 192, 192);
-				p.fillRect(QRect(bar_x, third_bar_y, bar_width, third_bar_height), QColor(19, 19, 19));
-				break;
-			}
-			p.fillRect(QRect(bar_x, 0, bar_width, first_bar_height), first_color);
-			p.fillRect(QRect(bar_x, first_bar_height, bar_width, second_bar_height), second_color);
-		}
-		for (int i=0;i<SMPTE_LOWER_BARS;i++) {
-			bar_x = third_bar_width*i;
-			switch (i) {
-			case 0: third_color = QColor(0, 33, 76); break;
-			case 1: third_color = QColor(255, 255, 255); break;
-			case 2: third_color = QColor(50, 0, 106); break;
-			case 3: third_color = QColor(19, 19, 19); break;
-			}
-			p.fillRect(QRect(bar_x, third_bar_y, third_bar_width, third_bar_height), third_color);
-		}
-		break;
-	}
-}*/
+void SolidEffect::ui_update(int i) {
+    solid_color_field->set_enabled(i == SOLID_TYPE_COLOR);
+    checkerboard_size_field->set_enabled(i == SOLID_TYPE_CHECKERBOARD);
+}
--- effects/internal/solideffect.h
+++ effects/internal/solideffect.h
@@ -9,12 +9,15 @@ class QOpenGLTexture;
 class SolidEffect : public Effect {
 	Q_OBJECT
 public:
-	SolidEffect(Clip* c, const EffectMeta *em);
-	EffectField* solid_type;
-	EffectField* solid_color_field;
-	EffectField* opacity_field;
-    EffectField* solid_width_field;
+	SolidEffect(Clip* c, const EffectMeta *em);	
 	void redraw(double timecode);
+private slots:
+    void ui_update(int);
+private:
+    EffectField* solid_type;
+    EffectField* solid_color_field;
+    EffectField* opacity_field;
+    EffectField* checkerboard_size_field;
 };
 
 #endif // SOLIDEFFECT_H
