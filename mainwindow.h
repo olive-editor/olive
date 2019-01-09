59417edd8758f8f694bf01eb07854a9f40ea778c
Jonathan Noble <jonnobleuk@gmail.com>
2019-01-09 21:20:53 +0000

    default member inits. overloaded virtual funcs

--- mainwindow.h
+++ mainwindow.h
@@ -30,7 +30,7 @@ class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget *parent = 0);
     void updateTitle(const QString &url);
-    ~MainWindow();
+    virtual ~MainWindow();
 
     void launch_with_project(const char *s);
 
@@ -50,8 +50,8 @@ public slots:
     void toggle_full_screen();
 
 protected:
-    void closeEvent(QCloseEvent *);
-    void paintEvent(QPaintEvent *event);
+    virtual void closeEvent(QCloseEvent *);
+    virtual void paintEvent(QPaintEvent *event);
 
 private slots:
     void clear_undo_stack();
@@ -141,62 +141,62 @@ private:
     bool can_close_project();
     void setup_menus();
 
-    // menu bar menus
-    QMenu* window_menu;
-
-    // file menu actions
-    QMenu* open_recent;
-
-    // view menu actions
-    QAction* track_lines;
-    QAction* frames_action;
-    QAction* drop_frame_action;
-    QAction* nondrop_frame_action;
-    QAction* milliseconds_action;
-    QAction* no_autoscroll;
-    QAction* page_autoscroll;
-    QAction* smooth_autoscroll;
-    QAction* title_safe_off;
-    QAction* title_safe_default;
-    QAction* title_safe_43;
-    QAction* title_safe_169;
-    QAction* title_safe_custom;
-    QAction* full_screen;
-    QAction* show_all;
-
-    // tool menu actions
-    QAction* pointer_tool_action;
-    QAction* edit_tool_action;
-    QAction* ripple_tool_action;
-    QAction* razor_tool_action;
-    QAction* slip_tool_action;
-    QAction* slide_tool_action;
-    QAction* hand_tool_action;
-    QAction* transition_tool_action;
-    QAction* snap_toggle;
-    QAction* selecting_also_seeks;
-    QAction* edit_tool_also_seeks;
-    QAction* edit_tool_selects_links;
-    QAction* seek_to_end_of_pastes;
-    QAction* scroll_wheel_zooms;
-    QAction* rectified_waveforms;
-    QAction* enable_drag_files_to_timeline;
-    QAction* autoscale_by_default;
-    QAction* enable_seek_to_import;
-    QAction* enable_audio_scrubbing;
-    QAction* enable_drop_on_media_to_replace;
-    QAction* enable_hover_focus;
-    QAction* set_name_and_marker;
-
-    // edit menu actions
-    QAction* undo_action;
-    QAction* redo_action;
-
     void set_bool_action_checked(QAction* a);
     void set_int_action_checked(QAction* a, const int& i);
     void set_button_action_checked(QAction* a);
 
-    bool enable_launch_with_project;
+    // menu bar menus
+    QMenu* window_menu = NULL;
+
+    // file menu actions
+    QMenu* open_recent = NULL;
+
+    // view menu actions
+    QAction* track_lines = NULL;
+    QAction* frames_action = NULL;
+    QAction* drop_frame_action = NULL;
+    QAction* nondrop_frame_action = NULL;
+    QAction* milliseconds_action = NULL;
+    QAction* no_autoscroll = NULL;
+    QAction* page_autoscroll = NULL;
+    QAction* smooth_autoscroll = NULL;
+    QAction* title_safe_off = NULL;
+    QAction* title_safe_default = NULL;
+    QAction* title_safe_43 = NULL;
+    QAction* title_safe_169 = NULL;
+    QAction* title_safe_custom = NULL;
+    QAction* full_screen = NULL;
+    QAction* show_all = NULL;
+
+    // tool menu actions
+    QAction* pointer_tool_action = NULL;
+    QAction* edit_tool_action = NULL;
+    QAction* ripple_tool_action = NULL;
+    QAction* razor_tool_action = NULL;
+    QAction* slip_tool_action = NULL;
+    QAction* slide_tool_action = NULL;
+    QAction* hand_tool_action = NULL;
+    QAction* transition_tool_action = NULL;
+    QAction* snap_toggle = NULL;
+    QAction* selecting_also_seeks = NULL;
+    QAction* edit_tool_also_seeks = NULL;
+    QAction* edit_tool_selects_links = NULL;
+    QAction* seek_to_end_of_pastes = NULL;
+    QAction* scroll_wheel_zooms = NULL;
+    QAction* rectified_waveforms = NULL;
+    QAction* enable_drag_files_to_timeline = NULL;
+    QAction* autoscale_by_default = NULL;
+    QAction* enable_seek_to_import = NULL;
+    QAction* enable_audio_scrubbing = NULL;
+    QAction* enable_drop_on_media_to_replace = NULL;
+    QAction* enable_hover_focus = NULL;
+    QAction* set_name_and_marker = NULL;
+
+    // edit menu actions
+    QAction* undo_action = NULL;
+    QAction* redo_action = NULL;
+
+    bool enable_launch_with_project = false;
 };
 
 extern MainWindow* mainWindow;
