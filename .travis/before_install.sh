7877cfe397488ea1e76e422db9039aadb8221a37
itsmattkc <itsmattkc@gmail.com>
2019-02-10 13:16:16 -0800

    bash to source in travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-	brew install ffmpeg qt5
+	# do nothing
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
 	sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
