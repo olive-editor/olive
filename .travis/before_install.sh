9a20fa8ba54a3a4b8e1be5dd870869e22a0cde78
itsmattkc <itsmattkc@gmail.com>
2019-02-10 13:29:55 -0800

    reworded before_install

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+#if [[ "$TRAVIS_OS_NAME" == "osx" ]]
+#then
 	# do nothing
-elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+#elif [[ "$TRAVIS_OS_NAME" == "linux" ]]
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then
 	sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
 	sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
 	sudo apt-get update -qq
