487c011603ea47e375175f742d3ad0bf0d9403ad
itsmattkc <itsmattkc@gmail.com>
2019-02-10 13:20:35 -0800

    wrap os name in quotes

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 	lrelease olive.pro
 	qmake CONFIG+=release PREFIX=/usr
 	make -j$(nproc)
 	macdeployqt Olive.app
 	zip -r Olive-$(git rev-parse --short HEAD)-macOS.zip Olive.app
-elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	lrelease olive.pro
 	if [ "$ARCH" == "i386" ]; then
 		qmake CONFIG+=release "QMAKE_CFLAGS+=-m32" "QMAKE_CXXFLAGS+=-m32" "QMAKE_LFLAGS+=-m32" PREFIX=/usr -spec linux-g++-32; fi
@@ -24,7 +24,7 @@ elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 		rm Olive*.AppImage
 		wget -c -nv "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-i686.AppImage"
 		chmod a+x appimagetool-i686.AppImage
-		./appimagetool-i686.AppImage 'appdir' -n -g
+		./appimagetool-i686.AppImage "appdir" -n -g
 	fi
 fi
 
