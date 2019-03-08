a5dfd25ecbf6a10965dbca2aa11911719cd7ad7f
itsmattkc <itsmattkc@gmail.com>
2019-03-08 22:14:30 +1100

    use PR blocking code on all platforms

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -3,6 +3,12 @@
 # retrieve upload tool
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
 
+# only create release for master
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+	export TRAVIS_EVENT_TYPE=pull_request
+fi
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
 	# upload final package
@@ -11,12 +17,6 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
 	find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
-
-	# only create release for master
-	if [ "$TRAVIS_BRANCH" != "master" ]
-	then
-		export TRAVIS_EVENT_TYPE=pull_request
-	fi
 
 	# upload final package
 	bash upload.sh Olive*.AppImage*
