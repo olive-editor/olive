359f937b73d71ffdef3b676e2f66191179e033aa
itsmattkc <itsmattkc@gmail.com>
2019-03-21 10:41:10 +1100

    fixed #656

--- effects/fields/stringfield.cpp
+++ effects/fields/stringfield.cpp
@@ -10,7 +10,8 @@ StringField::StringField(EffectRow* parent, const QString& id, bool rich_text) :
   EffectField(parent, id, EFFECT_FIELD_STRING),
   rich_text_(rich_text)
 {
-
+  // Set default value to an empty string
+  SetValueAt(0, "");
 }
 
 QString StringField::GetStringAt(double timecode)
