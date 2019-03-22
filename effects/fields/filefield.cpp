cc69f5b326b7192d09b7bf1e9728c7128790403d
itsmattkc <itsmattkc@gmail.com>
2019-03-23 03:04:58 +1100

    set file field to default empty string

--- effects/fields/filefield.cpp
+++ effects/fields/filefield.cpp
@@ -7,7 +7,8 @@
 FileField::FileField(EffectRow* parent, const QString &id) :
   EffectField(parent, id, EFFECT_FIELD_FILE)
 {
-
+  // Set default value to an empty string
+  SetValueAt(0, "");
 }
 
 QString FileField::GetFileAt(double timecode)
