55c869ac1b1ef7988e9b21c1cbd399ea8320a905
itsmattkc <itsmattkc@gmail.com>
2019-06-22 23:26:31 -0700

    added copyright header to all source files

--- app/widget/menu/menushared.h
+++ app/widget/menu/menushared.h
@@ -1,5 +1,25 @@
-#ifndef MENU_H
-#define MENU_H
+/***
+
+  Olive - Non-Linear Video Editor
+  Copyright (C) 2019 Olive Team
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
+#ifndef MENUSHARED_H
+#define MENUSHARED_H
 
 #include "widget/menu/menu.h"
 
@@ -49,4 +69,4 @@ namespace olive {
 extern MenuShared menu_shared;
 }
 
-#endif // MENU_H
+#endif // MENUSHARED_H
