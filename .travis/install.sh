d1ce224e8bab68df174b7e60da15c76e95d2d586
itsmattkc <itsmattkc@gmail.com>
2019-09-10 14:26:04 +1000

    added missing libs for travis trusty install

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,12 +2,12 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
-    brew install ffmpeg qt5 grep
+    brew install ffmpeg qt5 grep opencolorio openimageio
     export PATH="/usr/local/opt/qt/bin:/usr/local/opt/python@2/libexec/bin:$PATH"
 
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-    sudo apt-get -y install qt59base qt59multimedia qt59svg libavformat-dev libavcodec-dev libavfilter-dev libavutil-dev libswscale-dev libswresample-dev cmake
+    sudo apt-get -y install qt59base qt59multimedia qt59svg libavformat-dev libavcodec-dev libavfilter-dev libavutil-dev libswscale-dev libswresample-dev cmake libopencolorio-dev libopenimageio-dev
     source /opt/qt*/bin/qt*-env.sh
 
 fi
--- .travis/script.sh
+++ .travis/script.sh
@@ -2,43 +2,43 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
-	# Generate Makefile
-	cmake .
+    # Generate Makefile
+    cmake .
 
-	# Make
-	make -j$(sysctl -n hw.ncpu)
+    # Make
+    make -j$(sysctl -n hw.ncpu)
 
-	# Move Qt deps into bundle
-	macdeployqt Olive.app
+    # Move Qt deps into bundle
+    macdeployqt Olive.app
 
-	# Fix other deps that macdeployqt missed
-	wget -c -nv https://github.com/arl/macdeployqtfix/raw/master/macdeployqtfix.py
-	python2 macdeployqtfix.py Olive.app/Contents/MacOS/Olive /usr/local/Cellar/qt5/5.*/
+    # Fix other deps that macdeployqt missed
+    wget -c -nv https://github.com/arl/macdeployqtfix/raw/master/macdeployqtfix.py
+    python2 macdeployqtfix.py Olive.app/Contents/MacOS/Olive /usr/local/Cellar/qt5/5.*/
 
-	# Distribute in zip
-	zip -r Olive-$(git rev-parse --short HEAD)-macOS.zip Olive.app
+    # Distribute in zip
+    zip -r Olive-$(git rev-parse --short HEAD)-macOS.zip Olive.app
 
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-	# Generate Makefile
-	cmake .
+    # Generate Makefile
+    cmake .
 
-	# Make
-	make -j$(nproc)
+    # Make
+    make -j$(nproc)
 
-	# Use `make install` on `appdir` to place files in the correct place
-	make INSTALL_ROOT=appdir -j$(nproc) install ; find appdir/
+    # Use `make install` on `appdir` to place files in the correct place
+    make INSTALL_ROOT=appdir -j$(nproc) install ; find appdir/
 
-	# Download linuxdeployqt
-	wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-	chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+    # Download linuxdeployqt
+    wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+    chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 
-	unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-	
-	# linuxdeployqt uses this for naming the file
-	export VERSION=$(git rev-parse --short HEAD)
+    unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+    
+    # linuxdeployqt uses this for naming the file
+    export VERSION=$(git rev-parse --short HEAD)
 
-	# Use linuxdeployqt to set up dependencies
-	./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -extra-plugins=imageformats/libqsvg.so -appimage
+    # Use linuxdeployqt to set up dependencies
+    ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -extra-plugins=imageformats/libqsvg.so -appimage
 
 fi
