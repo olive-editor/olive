0cda23e6f7d442af7aa5dc4171f70e4d55bf069f
itsmattkc <itsmattkc@gmail.com>
2019-09-11 17:44:16 +1000

    updated travis for new oiio ppa

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,10 +2,17 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-    # install apt repos necessary for Olive
+    # Qt 5.9.3 - FIXME: Consider finding newer version
     sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
+
+    # FFmpeg 4.x (uses libs from 3.x repo)
     sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
     sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y
+
+    # OpenImageIO
+    sudo add-apt-repository ppa:olive-editor/openimageio -y
+
+    # Update apt
     sudo apt-get update -qq
 
 fi
