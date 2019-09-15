6183f5b6a544193f9442432c0b97e7e31f89bdde
itsmattkc <itsmattkc@gmail.com>
2019-09-15 14:37:06 +1000

    don't reinstall packages that already exist

--- appveyor-build.sh
+++ appveyor-build.sh
@@ -5,7 +5,7 @@ ARCH=Win64
 pacman -Syu --noconfirm
 
 # Install necessary packages
-pacman -S --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-qt5 mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-openimageio mingw-w64-x86_64-opencolorio 
+pacman -S --noconfirm --needed mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-qt5 mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-openimageio mingw-w64-x86_64-opencolorio 
 
 # Generate Ninja Makefiles
 cmake . -G "Ninja"
