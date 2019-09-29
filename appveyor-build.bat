2b3659f1532032bc42a03cf6faa55681950f453c
itsmattkc <itsmattkc@gmail.com>
2019-09-30 06:47:11 +1000

    add ability to enable debugging through appveyor

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -3,3 +3,8 @@ set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 
 REM Run MSYS script
 bash -c ./appveyor-build.sh
+
+REM Check if this build should set up a debugging session
+IF "%ENABLE_RDP%"=="1" (
+    powershell $blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+)
