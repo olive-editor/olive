31453e7b46f50b71518a05799d11373aabb1d0fb
itsmattkc <itsmattkc@gmail.com>
2019-10-17 09:43:09 +1100

    stop when pushing samples as well

--- app/audio/audiohybriddevice.cpp
+++ app/audio/audiohybriddevice.cpp
@@ -1,3 +1,23 @@
+/***
+
+  Olive - Non-Linear Video Editor
+  Copyright (C) 2019 Olive Team
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
 #include "audiohybriddevice.h"
 
 #include <QDebug>
@@ -13,6 +33,8 @@ AudioHybridDevice::AudioHybridDevice(QObject *parent) :
 
 void AudioHybridDevice::Push(const QByteArray& samples)
 {
+  Stop();
+
   pushed_samples_ = samples;
   sample_index_ = 0;
 
