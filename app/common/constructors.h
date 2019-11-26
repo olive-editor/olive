2889811b7b2c5c037791fa1400ae7e858b8addab
itsmattkc <itsmattkc@gmail.com>
2019-11-27 09:50:30 +1000

    improved documentation on DISABLE_COPY_MOVE macros

--- app/common/constructors.h
+++ app/common/constructors.h
@@ -2,12 +2,13 @@
 #define CONSTRUCTORS_H
 
 /**
- * Copy/move deleters. Similar to Q_DISABLE_COPY_MOVE, et al. but those functions
+ * Copy/move deleters. Similar to Q_DISABLE_COPY_MOVE, et al. but those functions are not present in Qt < 5.13 so we
+ * use our own functions for portability.
  */
 
 #define DISABLE_COPY(Class) \
-    Class(const Class &) = delete;\
-    Class &operator=(const Class &) = delete;
+  Class(const Class &) = delete;\
+  Class &operator=(const Class &) = delete;
 
 #define DISABLE_MOVE(Class) \
   Class(Class &&) = delete; \
