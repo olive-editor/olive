c1b96927a294ecb0b834de50c6e8f8e6f28789ef
itsmattkc <itsmattkc@gmail.com>
2019-12-09 23:46:15 +1100

    this files were actually renamed in an earlier commit but i forgot to add them

--- app/node/block/transition/crossdissolve/crossdissolvetransition.cpp
+++
@@ -1,6 +0,0 @@
-#include "crossdissolvetransition.h"
-
-CrossDissolveTransition::CrossDissolveTransition()
-{
-
-}
--- app/node/block/transition/diptoblack/diptoblacktransition.cpp
+++
@@ -1,6 +0,0 @@
-#include "diptoblacktransition.h"
-
-DipToBlackTransition::DipToBlackTransition()
-{
-
-}
