f82bef4195b2f4fbec53d9e5ad09eee87561403d
Tércio Martins <tecnotercio@yahoo.com.br>
2019-12-12 03:12:28 -0300

    Fix Travis script "install.sh" - execution hangs on osx "if" condition.

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-
-    #export PATH="/usr/local/opt/qt/bin:/usr/local/opt/python@2/libexec/bin:$PATH"
-
-elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
     sudo apt-get -y -o Dpkg::Options::="--force-overwrite" install qt510base qt510multimedia qt510svg qttools5-dev libavformat-dev libavcodec-dev libavfilter-dev libavutil-dev libswscale-dev libswresample-dev cmake libopencolorio-dev libopenimageio-dev libopenexr-dev
     source /opt/qt*/bin/qt*-env.sh
+
+#elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+
+    #export PATH="/usr/local/opt/qt/bin:/usr/local/opt/python@2/libexec/bin:$PATH"
 
 fi
