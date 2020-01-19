a2aee3cf5c24477a89c1802e356425bced4eb72c
itsmattkc <itsmattkc@gmail.com>
2020-01-20 01:23:55 +1100

    appveyor: get 64-bit packages and use vcpkg file in cmake

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -1,6 +1,9 @@
 REM Set up Visual Studio x64 environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+REM Install 64-bit
+set VCPKG_DEFAULT_TRIPLET=x64-windows
+
 REM Install Open*IO libraries
 vcpkg install opencolorio
 vcpkg install openimageio
@@ -9,10 +12,9 @@ REM Integrate libraries
 cd c:\tools\vcpkg
 vcpkg integrate install
 cd %APPVEYOR_BUILD_FOLDER%
-cmake -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake ...
 
 REM Run cmake
-cmake -G "NMake Makefiles" .
+cmake -G "NMake Makefiles" . -DCMAKE_TOOLCHAIN_FILE=c:/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 REM Build with JOM
 C:\Qt\Tools\QtCreator\bin\jom.exe
