1c5a11b80b8fb796323eda75493490ee7c23294c
itsmattkc <itsmattkc@gmail.com>
2020-01-20 00:50:32 +1100

    appveyor: use jom and integrate vcpkg packages

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -5,11 +5,17 @@ REM Install Open*IO libraries
 vcpkg install opencolorio
 vcpkg install openimageio
 
+REM Integrate libraries
+cd c:\tools\vcpkg
+vcpkg integrate install
+cd %APPVEYOR_BUILD_FOLDER%
+cmake -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake ...
+
 REM Run cmake
 cmake -G "NMake Makefiles" .
 
-REM Build
-nmake
+REM Build with JOM
+C:\Qt\Tools\QtCreator\bin\jom.exe
 
 REM Check if this build should set up a debugging session
 IF "%ENABLE_RDP%"=="1" (
