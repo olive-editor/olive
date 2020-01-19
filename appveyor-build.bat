649ee04eef4cec044a9279e935443cc65cdc8cbd
itsmattkc <itsmattkc@gmail.com>
2020-01-20 03:33:17 +1100

    appveyor: acquire ffmpeg from zeranoe and include

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -13,8 +13,15 @@ cd c:\tools\vcpkg
 vcpkg integrate install
 cd %APPVEYOR_BUILD_FOLDER%
 
-REM Add Qt directory to path
-set PATH=%PATH%;C:\Qt\5.13.2\msvc2017_64
+REM Acquire FFmpeg
+set FFMPEG_VER=ffmpeg-4.2.1-win64
+curl https://ffmpeg.zeranoe.com/builds/win64/dev/%FFMPEG_VER%-dev.zip > %FFMPEG_VER%-dev.zip
+curl https://ffmpeg.zeranoe.com/builds/win64/shared/%FFMPEG_VER%-shared.zip > %FFMPEG_VER%-shared.zip
+7z x %FFMPEG_VER%-dev.zip
+7z x %FFMPEG_VER%-shared.zip
+
+REM Add Qt and FFmpeg directory to path
+set PATH=%PATH%;C:\Qt\5.13.2\msvc2017_64\bin;%APPVEYOR_BUILD_FOLDER%\%FFMPEG_VER%-dev
 
 REM Run cmake
 cmake -G "NMake Makefiles" . -DCMAKE_TOOLCHAIN_FILE=c:/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake
