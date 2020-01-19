8f4910a7c8190c1675fd2434e85b01e6f3de86e3
itsmattkc <itsmattkc@gmail.com>
2020-01-19 22:20:30 +1100

    travis: update linux dep install code

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,12 +5,14 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     # Qt 5.10.1 (may have to source newer version at some point)
     sudo add-apt-repository ppa:beineri/opt-qt-5.10.1-trusty -y
 
-    # FFmpeg 4.x (uses libs from 3.x repo)
-    sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
+    # FFmpeg 4.x
     sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y
 
     # OpenImageIO
     sudo add-apt-repository ppa:olive-editor/openimageio -y
+
+    # OpenColorIO
+    sudo add-apt-repository ppa:olive-editor/opencolorio -y
 
     # Update apt
     sudo apt-get update -qq
