78c504d292e6bff271907672b4fc48b077cebb12
itsmattkc <itsmattkc@gmail.com>
2020-03-18 19:09:13 +1100

    footageviewer: remove unnecessary cache invalidations
    Minor optimization, now that the FootageViewer's nodes are
    connected/disconnected as part of the process, the second call to invalidate
    is unnecessary and will only serve to re-invalidate actually valid cache data.

--- app/widget/viewer/footageviewer.cpp
+++ app/widget/viewer/footageviewer.cpp
@@ -64,8 +64,6 @@ void FootageViewerWidget::SetFootage(Footage *footage)
     }
 
     ConnectViewerNode(viewer_node_, footage->project()->color_manager());
-    video_renderer_->InvalidateCache(TimeRange(0, viewer_node_->Length()));
-    audio_renderer_->InvalidateCache(TimeRange(0, viewer_node_->Length()));
   }
 }
 
