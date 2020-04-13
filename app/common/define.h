5c927ce7a44645f39dd8ce246715164f7ceee989
itsmattkc <itsmattkc@gmail.com>
2020-04-13 23:43:46 +1000

    core: merged define and constructor headers

--- app/common/define.h
+++ app/common/define.h
@@ -49,4 +49,21 @@ OLIVE_NAMESPACE_EXIT
 
 #define OLIVE_NS_ARG(x, y) QArgument<OLIVE_NAMESPACE::x>(MACRO_VAL_AS_STR(OLIVE_NAMESPACE) "::" #x, y)
 
+/**
+ * Copy/move deleters. Similar to Q_DISABLE_COPY_MOVE, et al. but those functions are not present in Qt < 5.13 so we
+ * use our own functions for portability.
+ */
+
+#define DISABLE_COPY(Class) \
+  Class(const Class &) = delete;\
+  Class &operator=(const Class &) = delete;
+
+#define DISABLE_MOVE(Class) \
+  Class(Class &&) = delete; \
+  Class &operator=(Class &&) = delete;
+
+#define DISABLE_COPY_MOVE(Class) \
+  DISABLE_COPY(Class) \
+  DISABLE_MOVE(Class)
+
 #endif // OLIVECOMMONDEFINE_H
