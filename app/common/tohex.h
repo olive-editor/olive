df534fadbee8207f981cbb5549c7248a57fee7d4
itsmattkc <itsmattkc@gmail.com>
2020-04-16 23:13:35 +1000

    common: added function to convert any number to a hex string
    Helps with debugging.

--- app/common/tohex.h
+++ app/common/tohex.h
@@ -1,4 +1,16 @@
 #ifndef TOHEX_H
 #define TOHEX_H
 
+#include <QtGlobal>
+
+#include "common/define.h"
+
+OLIVE_NAMESPACE_ENTER
+
+inline QString ToHex(quint64 t) {
+  return QStringLiteral("%1").arg(t, 0, 16);
+}
+
+OLIVE_NAMESPACE_EXIT
+
 #endif // TOHEX_H
