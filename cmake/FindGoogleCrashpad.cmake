bbec9fcb05973dd23a28274be1e0f1d97ec1acfc
Simran Spiller <simran.spiller@gmail.com>
2020-09-22 23:43:19 +0200

    Formatting

--- cmake/FindGoogleCrashpad.cmake
+++ cmake/FindGoogleCrashpad.cmake
@@ -18,46 +18,46 @@ include(FindPackageHandleStandardArgs)
 
 # Try to find include files
 find_path(CRASHPAD_CLIENT_INCLUDE_DIR
-        client/crashpad_client.h
-    HINTS
-        "${CRASHPAD_LOCATION}"
-        "$ENV{CRASHPAD_LOCATION}"
-        "${CRASHPAD_BASE_DIR}"
+    client/crashpad_client.h
+  HINTS
+    "${CRASHPAD_LOCATION}"
+    "$ENV{CRASHPAD_LOCATION}"
+    "${CRASHPAD_BASE_DIR}"
 )
 list(APPEND CRASHPAD_INCLUDE_DIRS ${CRASHPAD_CLIENT_INCLUDE_DIR})
 
 find_path(CRASHPAD_BASE_INCLUDE_DIR
-        base/files/file_path.h
-    HINTS
-        "${CRASHPAD_LOCATION}"
-        "$ENV{CRASHPAD_LOCATION}"
-        "${CRASHPAD_BASE_DIR}"
-    PATH_SUFFIXES
-        "third_party/mini_chromium/mini_chromium"
+    base/files/file_path.h
+  HINTS
+    "${CRASHPAD_LOCATION}"
+    "$ENV{CRASHPAD_LOCATION}"
+    "${CRASHPAD_BASE_DIR}"
+  PATH_SUFFIXES
+    "third_party/mini_chromium/mini_chromium"
 )
 list(APPEND CRASHPAD_INCLUDE_DIRS ${CRASHPAD_BASE_INCLUDE_DIR})
 
 # Try to find build files
 if (WIN32)
   find_path(CRASHPAD_LIBRARY_DIRS
-          obj/client/client.lib
-      HINTS
-          "${CRASHPAD_LOCATION}"
-          "$ENV{CRASHPAD_LOCATION}"
-          "${CRASHPAD_BASE_DIR}"
-      PATH_SUFFIXES
-          "out/Default"
+      obj/client/client.lib
+    HINTS
+      "${CRASHPAD_LOCATION}"
+      "$ENV{CRASHPAD_LOCATION}"
+      "${CRASHPAD_BASE_DIR}"
+    PATH_SUFFIXES
+      "out/Default"
   )
 elseif(UNIX)
   # Assuming macOS works this way, don't actually know
   find_path(CRASHPAD_LIBRARY_DIRS
-          obj/client/libclient.a
-      HINTS
-          "${CRASHPAD_LOCATION}"
-          "$ENV{CRASHPAD_LOCATION}"
-          "${CRASHPAD_BASE_DIR}"
-      PATH_SUFFIXES
-          "out/Default"
+      obj/client/libclient.a
+    HINTS
+      "${CRASHPAD_LOCATION}"
+      "$ENV{CRASHPAD_LOCATION}"
+      "${CRASHPAD_BASE_DIR}"
+    PATH_SUFFIXES
+      "out/Default"
   )
 endif()
 
@@ -72,8 +72,9 @@ foreach (COMPONENT ${_crashpad_components})
   string(TOUPPER ${SHORT_COMPONENT} UPPER_COMPONENT)
 
   find_library(CRASHPAD_${UPPER_COMPONENT}_LIB
-                ${SHORT_COMPONENT}
-                HINTS "${CRASHPAD_LIBRARY_DIRS}/obj/${COMPONENT}"
+      ${SHORT_COMPONENT}
+    HINTS
+      "${CRASHPAD_LIBRARY_DIRS}/obj/${COMPONENT}"
   )
 
   list(APPEND CRASHPAD_LIBRARIES ${CRASHPAD_${UPPER_COMPONENT}_LIB})
