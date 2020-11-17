d241090a9f262dad2e9bc80725368e9286f90434
itsmattkc <itsmattkc@gmail.com>
2020-11-17 20:13:22 +1100

    finally updated the copyright year

--- app/dialog/projectproperties/projectproperties.h
+++ app/dialog/projectproperties/projectproperties.h
@@ -1,7 +1,7 @@
 /***
   
   Olive - Non-Linear Video Editor
-  Copyright (C) 2019 Olive Team
+  Copyright (C) 2020 Olive Team
   
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
