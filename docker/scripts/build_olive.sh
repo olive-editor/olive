ab49fd8d7cfbcb02cfb80c4c0ef5844417249e0a
Simran Spiller <simran.spiller@gmail.com>
2021-04-07 01:13:49 +0200

    CI: Exclude (probably) unused Qt libs

--- docker/scripts/build_olive.sh
+++ docker/scripts/build_olive.sh
@@ -9,9 +9,21 @@ cmake --build .
 
 cmake --install app --prefix appdir/usr
 
+# TODO: Can the following libs be excluded?
+#libQt5DBus.so,\
+#libQt5MultimediaGstTools.so,\
+#libQt5MultimediaWidgets.so,\
+
 /usr/local/linuxdeployqt-x86_64.AppImage \
   appdir/usr/share/applications/org.olivevideoeditor.Olive.desktop \
   -appimage \
+  -exclude-libs=\
+libQt5Pdf.so,\
+libQt5Qml.so,\
+libQt5QmlModels.so,\
+libQt5Quick.so,\
+libQt5Svg.so,\
+libQt5VirtualKeyboard.so \
   -bundle-non-qt-libs \
   -executable=appdir/usr/bin/crashpad_handler \
   -executable=appdir/usr/bin/minidump_stackwalk \
