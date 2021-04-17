77d4b18a60069d6021f2320f82be80ecf0f3740a
itsmattkc <itsmattkc@gmail.com>
2021-04-17 18:56:43 +1000

    updated copyright year for 2021 [skip ci]

--- app/widget/scope/waveform/waveform.cpp
+++ app/widget/scope/waveform/waveform.cpp
@@ -2,7 +2,7 @@
 /***
 
   Olive - Non-Linear Video Editor
-  Copyright (C) 2020 Olive Team
+  Copyright (C) 2021 Olive Team
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
