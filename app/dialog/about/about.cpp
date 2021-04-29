af742e6e62f1319ff90da6232f07a3c59b36ce72
itsmattkc <itsmattkc@gmail.com>
2021-04-29 20:54:15 +1000

    aboutdialog: support patreon list

--- app/dialog/about/about.cpp
+++ app/dialog/about/about.cpp
@@ -25,6 +25,10 @@
 #include <QLabel>
 #include <QVBoxLayout>
 
+#include "common/qtutils.h"
+#include "patreon.h"
+#include "scrollinglabel.h"
+
 namespace olive {
 
 AboutDialog::AboutDialog(QWidget *parent) :
@@ -33,38 +37,58 @@ AboutDialog::AboutDialog(QWidget *parent) :
   setWindowTitle(tr("About %1").arg(QApplication::applicationName()));
 
   QVBoxLayout* layout = new QVBoxLayout(this);
-  //layout->setSpacing(20);
+
+  layout->addWidget(new QLabel());
+
+  QLabel* icon = new QLabel(QStringLiteral("<html><img src=':/graphics/olive-splash.png'></html>"));
+  icon->setAlignment(Qt::AlignCenter);
+  layout->addWidget(icon);
 
   // Construct About text
   QLabel* label =
       new QLabel(QStringLiteral("<html><head/><body>"
-                                "<p><img src=\":/graphics/olive-splash.png\"/></p>"
-                                "<p><a href=\"https://www.olivevideoeditor.org/\">"
-                                "<span style=\" text-decoration: underline; color:#007af4;\">"
-                                "https://www.olivevideoeditor.org/"
-                                "</span></a></p>"
                                 "<p><b>%1</b> %2</p>" // AppName (version identifier)
+                                "<p><a href=\"https://www.olivevideoeditor.org/\">"
+                                "https://www.olivevideoeditor.org/"
+                                "</a></p>"
                                 "<p>%3</p>" // First statement
-                                "<p>%4</p>" // Second statement
                                 "</body></html>").arg(QApplication::applicationName(),
                                                       QApplication::applicationVersion(),
-                                                      tr("Olive is a non-linear video editor. This software is free and "
-                                                         "protected by the GNU GPL."),
-                                                      tr("Olive Team is obliged to inform users that Olive source code is "
-                                                         "available for download from its website.")),this);
+                                                      tr("Olive is a free open source non-linear video editor. "
+                                                         "This software is licensed under the GNU GPL Version 3.")));
 
   // Set text formatting
   label->setAlignment(Qt::AlignCenter);
-  label->setTextInteractionFlags(Qt::TextSelectableByMouse);
-  label->setCursor(Qt::IBeamCursor);
   label->setWordWrap(true);
+  label->setOpenExternalLinks(true);
+  label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
   layout->addWidget(label);
+
+  // Patrons where necessary
+  if (!patrons.isEmpty()) {
+    layout->addWidget(new QLabel());
+
+    QLabel* support_lbl = new QLabel(tr("<html>Olive wouldn't be possible without the support of gracious "
+                                        "donations from <a href='https://www.patreon.com/olivevideoeditor'>Patreon</a></html>:"));
+    support_lbl->setWordWrap(true);
+    support_lbl->setAlignment(Qt::AlignCenter);
+    support_lbl->setOpenExternalLinks(true);
+    layout->addWidget(support_lbl);
+
+    ScrollingLabel* scroll = new ScrollingLabel(patrons);
+    scroll->StartAnimating();
+    layout->addWidget(scroll);
+  }
+
+  layout->addWidget(new QLabel());
 
   QDialogButtonBox* buttons = new QDialogButtonBox(QDialogButtonBox::Ok, this);
   buttons->setCenterButtons(true);
   layout->addWidget(buttons);
 
   connect(buttons, &QDialogButtonBox::accepted, this, &QDialog::accept);
+
+  setFixedSize(sizeHint());
 }
 
 }
