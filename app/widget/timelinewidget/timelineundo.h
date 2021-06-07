olive-editor/olive

commit ef206e9532ac73cd10da0e965de43f5618f38a10

timeline: use operation when placing block
Fixes unnecessary length change of caches.

app/widget/timelinewidget/timelineundo.h
@@ -1370,6 +1370,8 @@ public:
 
     Track* track = timeline_->GetTrackAt(track_index_);
 
+    track->BeginOperation();
+
     bool append = (in_ >= track->track_length());
 
     // Check if the placement location is past the end of the timeline
@@ -1409,6 +1411,8 @@ public:
       }
     }
 
+    track->EndOperation();
+
     for (int i=0; i<position_commands_.size(); i++) {
       position_commands_.at(i)->redo();
     }
@@ -1423,6 +1427,7 @@ public:
     Track* t = timeline_->GetTrackAt(track_index_);
 
     // Firstly, remove our insert
+    t->BeginOperation();
     t->RippleRemoveBlock(insert_);
 
     if (ripple_remove_command_) {
@@ -1432,6 +1437,7 @@ public:
       t->RippleRemoveBlock(gap_);
       gap_->setParent(&memory_manager_);
     }
+    t->EndOperation();
 
     // Remove tracks if we added them
     for (int i=add_track_commands_.size()-1; i>=0; i--) {

commit 48828d6632a126c0967368eefd7114b9acebb076

timeline: invalidate after place operation
Fixes regression introduced in ef206e9532ac73cd10da0e965de43f5618f38a10
Fixes #1654

app/widget/timelinewidget/timelineundo.h
@@ -1413,6 +1413,10 @@ public:
 
     track->EndOperation();
 
+    if (ripple_remove_command_) {
+      track->Node::InvalidateCache(TimeRange(insert_->in(), insert_->out()), Track::kBlockInput);
+    }
+
     for (int i=0; i<position_commands_.size(); i++) {
       position_commands_.at(i)->redo();
     }
@@ -1426,6 +1430,8 @@ public:
 
     Track* t = timeline_->GetTrackAt(track_index_);
 
+    TimeRange insert_range(insert_->in(), insert_->out());
+
     // Firstly, remove our insert
     t->BeginOperation();
     t->RippleRemoveBlock(insert_);
@@ -1438,6 +1444,10 @@ public:
       gap_->setParent(&memory_manager_);
     }
     t->EndOperation();
+
+    if (ripple_remove_command_) {
+      t->Node::InvalidateCache(insert_range, Track::kBlockInput);
+    }
 
     // Remove tracks if we added them
     for (int i=add_track_commands_.size()-1; i>=0; i--) {