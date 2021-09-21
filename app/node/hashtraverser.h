748d909628cf2ad6114f1772029a5829feb0dbae
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-09-20 22:42:53 -0700

    hashtraverser: fixed missing override tags

--- app/node/hashtraverser.h
+++ app/node/hashtraverser.h
@@ -35,11 +35,11 @@ public:
 protected:
   virtual TexturePtr ProcessVideoFootage(const FootageJob &stream, const rational &input_time) override;
 
-  virtual SampleBufferPtr ProcessAudioFootage(const FootageJob &stream, const TimeRange &input_time);
+  virtual SampleBufferPtr ProcessAudioFootage(const FootageJob &stream, const TimeRange &input_time) override;
 
   virtual TexturePtr ProcessShader(const Node *node, const TimeRange &range, const ShaderJob& job) override;
 
-  virtual SampleBufferPtr ProcessSamples(const Node *node, const TimeRange &range, const SampleJob &job);
+  virtual SampleBufferPtr ProcessSamples(const Node *node, const TimeRange &range, const SampleJob &job) override;
 
   virtual TexturePtr ProcessFrameGeneration(const Node *node, const GenerateJob& job) override;
 
