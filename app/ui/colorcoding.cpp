cf1bd65b15b3f9acff06679fd6660044bed461b6
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-10-25 16:31:08 -0700

    colorcoding: added f suffix for floats

--- app/ui/colorcoding.cpp
+++ app/ui/colorcoding.cpp
@@ -23,22 +23,22 @@
 namespace olive {
 
 QVector<Color> ColorCoding::colors_ = {
-  Color(0.545, 0.255, 0.255),
-  Color(0.412, 0.188, 0.259),
-  Color(0.561, 0.427, 0.239),
-  Color(0.486, 0.306, 0.235),
-  Color(0.631, 0.612, 0.212),
-  Color(0.404, 0.478, 0.243),
-  Color(0.349, 0.576, 0.275),
-  Color(0.224, 0.459, 0.251),
-  Color(0.259, 0.471, 0.541),
-  Color(0.184, 0.376, 0.329),
-  Color(0.259, 0.365, 0.541),
-  Color(0.196, 0.216, 0.412),
-  Color(0.612, 0.294, 0.502),
-  Color(0.404, 0.220, 0.459),
-  Color(0.800, 0.800, 0.800),
-  Color(0.502, 0.502, 0.502)
+  Color(0.545f, 0.255f, 0.255f),
+  Color(0.412f, 0.188f, 0.259f),
+  Color(0.561f, 0.427f, 0.239f),
+  Color(0.486f, 0.306f, 0.235f),
+  Color(0.631f, 0.612f, 0.212f),
+  Color(0.404f, 0.478f, 0.243f),
+  Color(0.349f, 0.576f, 0.275f),
+  Color(0.224f, 0.459f, 0.251f),
+  Color(0.259f, 0.471f, 0.541f),
+  Color(0.184f, 0.376f, 0.329f),
+  Color(0.259f, 0.365f, 0.541f),
+  Color(0.196f, 0.216f, 0.412f),
+  Color(0.612f, 0.294f, 0.502f),
+  Color(0.404f, 0.220f, 0.459f),
+  Color(0.800f, 0.800f, 0.800f),
+  Color(0.502f, 0.502f, 0.502f)
 };
 
 QString ColorCoding::GetColorName(int c)
@@ -89,7 +89,7 @@ Color ColorCoding::GetColor(int c)
 
 Qt::GlobalColor ColorCoding::GetUISelectorColor(const Color &c)
 {
-  if (c.GetRoughLuminance() > 0.40) {
+  if (c.GetRoughLuminance() > 0.40f) {
     return Qt::black;
   } else {
     return Qt::white;
