29908f2d8b2f1b78a054647a780177d69cc1155f
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2022-05-29 11:13:05 -0700

    math: revised timestamp calculations around epsilon

--- app/common/timerange.cpp
+++ app/common/timerange.cpp
@@ -368,7 +368,7 @@ void TimeRangeListFrameIterator::UpdateIndexIfNecessary()
     range_index_++;
 
     if (range_index_ < list_.size()) {
-      current_ = Timecode::snap_time_to_timebase(list_.at(range_index_).in(), timebase_, Timecode::kRound);
+      current_ = Timecode::snap_time_to_timebase(list_.at(range_index_).in(), timebase_, Timecode::kCeil);
     }
   }
 }
