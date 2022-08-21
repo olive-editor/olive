e7799c9284b210e40f3ba6363584ba41b967dfa8
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2022-08-21 09:53:44 -0700

    projectexplorer: focus footage panel when opening media there

--- app/panel/project/project.cpp
+++ app/panel/project/project.cpp
@@ -174,7 +174,10 @@ void ProjectPanel::ItemDoubleClickSlot(Node *item)
     Core::instance()->DialogImportShow();
   } else if (dynamic_cast<Footage*>(item)) {
     // Open this footage in a FootageViewer
-    PanelManager::instance()->MostRecentlyFocused<FootageViewerPanel>()->ConnectViewerNode(static_cast<Footage*>(item));
+    auto panel = PanelManager::instance()->MostRecentlyFocused<FootageViewerPanel>();
+    panel->ConnectViewerNode(static_cast<Footage*>(item));
+    panel->raise();
+    panel->setFocus();
   } else if (dynamic_cast<Sequence*>(item)) {
     // Open this sequence in the Timeline
     Core::instance()->main_window()->OpenSequence(static_cast<Sequence*>(item));
