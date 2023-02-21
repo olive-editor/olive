32c8d08a10ff08d31c1fe27b0c7618158c19925a
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2023-02-21 15:02:13 -0800

    gizmos: draw shadow so they're still visible on white backdrop
    Fixes #2142

--- app/node/gizmo/path.cpp
+++ app/node/gizmo/path.cpp
@@ -30,9 +30,16 @@ PathGizmo::PathGizmo(QObject *parent) :
 
 void PathGizmo::Draw(QPainter *p) const
 {
+  // Draw transposed black
+  QPainterPath transposed = p->transform().map(path_);
+  transposed.translate(1, 1);
+  transposed = p->transform().inverted().map(transposed);
+  p->setPen(QPen(Qt::black, 0));
+  p->drawPath(transposed);
+
+  // Draw normal white polygon
   p->setPen(QPen(Qt::white, 0));
   p->setBrush(Qt::NoBrush);
-
   p->drawPath(path_);
 }
 
