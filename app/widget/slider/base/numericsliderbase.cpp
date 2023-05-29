f062b068a19497b76b21b1e8d06f6399ac80fd6d
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2023-05-29 12:14:48 -0700

    fix slider ladder on wayland

--- app/widget/slider/base/numericsliderbase.cpp
+++ app/widget/slider/base/numericsliderbase.cpp
@@ -52,17 +52,16 @@ void NumericSliderBase::SetDragMultiplier(const double &d)
 
 void NumericSliderBase::LabelPressed()
 {
-  // Generate width hint
   drag_ladder_ = new SliderLadder(drag_multiplier_, ladder_element_count_, GetFormattedValueToString(99999999));
   connect(drag_ladder_, &SliderLadder::DraggedByValue, this, &NumericSliderBase::LadderDragged);
   connect(drag_ladder_, &SliderLadder::Released, this, &NumericSliderBase::LadderReleased);
 
   drag_ladder_->SetValue(GetFormattedValueToString());
+  drag_ladder_->resize(drag_ladder_->sizeHint());
+  RepositionLadder();
   drag_ladder_->show();
 
   drag_start_value_ = GetValueInternal();
-
-  QMetaObject::invokeMethod(this, "RepositionLadder", Qt::QueuedConnection);
 }
 
 void NumericSliderBase::LadderDragged(int value, double multiplier)
